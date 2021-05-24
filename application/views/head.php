765069df7a5a5bedc05873ca17771dc36406b5f6
KANGPUNGYUN <zkdvnd@naver.com>
2021-05-24 12:02:03 +0900

    부트스트랩 참조 오류

--- application/views/head.php
+++ application/views/head.php
@@ -2,5 +2,8 @@
         <html>
             <head>
                 <meta charset="utf-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <!--Bootstrap-->
+                <link href="\application\static\lib\bootstrap\bootstrap-5.0.1-dist\css\bootstrap.min.css" rel="stylesheet" media="screen">
             </head>
         <body>

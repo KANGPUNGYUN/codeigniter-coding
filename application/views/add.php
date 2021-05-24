179b4fb091b1c3276d94f2c4130b950c342b6e3d
KANGPUNGYUN <zkdvnd@naver.com>
2021-05-24 10:18:23 +0900

    first commit

---
+++ application/views/add.php
@@ -0,0 +1,6 @@
+<form action="/index.php/topic/add" method="post">
+    <?php echo validation_errors(); ?>
+    <input type="text" name="title" placeholder="제목"/>
+    <textarea name="description" placeholder="본문" rows="15"></textarea>
+    <input type="submit" />
+</form>
---
+++ application/views/get.php
@@ -0,0 +1,11 @@
+    <article>
+        <h1><?=$topic->title?></h1>
+        <div>
+        <div>
+            <?=kdate($topic->created)?></div>
+            <?=auto_link($topic->description)?>
+        </div>
+    </article>
+    <div>
+        <a href='/index.php/topic/add' class="btn">추가</a>
+    </div>
